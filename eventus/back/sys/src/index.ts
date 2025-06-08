c13f3fc9e00528b7f8473259b874a9b62d17eec8
Kiryuuin <doraemonstichisnoon@gmail.com>
2025-06-09 00:18:58 +0700

    testing elysia

---
+++ eventus/back/sys/src/index.ts
@@ -0,0 +1,21 @@
+import swagger from "@elysiajs/swagger";
+import { logger } from "@tqman/nice-logger";
+import { Elysia, t } from "elysia";
+
+const app = new Elysia()
+  .use(swagger())
+  .use(logger())
+  .get("/", () => "amongus")
+  .get("/user/:id", ({ params: { id } }) => id, {
+    params: t.Object({
+      id: t.Numeric(),
+    }),
+  })
+  .post('/test', ({ body }) =>  {
+    return body
+  })
+  .listen(3000);
+
+console.log(
+  `Elysia at http://${app.server?.hostname}:${app.server?.port}`
+);
