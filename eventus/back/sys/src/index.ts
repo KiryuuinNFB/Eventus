0ef63d0f4c4e21f2eb48b8b716de4e1d1b507cea
KiryuuinNFB <doraemonstichisnoon@gmail.com>
2025-06-10 00:03:30 +0700

    hi this is micropost databayse

--- eventus/back/sys/src/index.ts
+++ eventus/back/sys/src/index.ts
@@ -1,6 +1,7 @@
 import swagger from "@elysiajs/swagger";
 import { logger } from "@tqman/nice-logger";
 import { Elysia, t } from "elysia";
+import { PrismaClient } from "@prisma/client";
 
 const app = new Elysia()
   .use(swagger())
