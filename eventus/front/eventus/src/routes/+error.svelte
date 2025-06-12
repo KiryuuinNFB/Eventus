5a129465577341d931fdd492d20e8d354252fe46
KiryuuinNFB <doraemonstichisnoon@gmail.com>
2025-06-12 13:24:22 +0700

    initialized some pages

---
+++ eventus/front/eventus/src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script>
+    let sitetitle = page.status + " " + page.error?.message;
+    import { page } from "$app/state";
+
+</script>
+
+<title>{sitetitle}</title>
+
+<div>
+    <h1>{page.status}</h1>
+    <h1>{page.error?.message}</h1>
+    <a href="/">Return to home page</a>
+</div>
---
+++ eventus/front/eventus/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+    <link rel="stylesheet" href="/themes/default.css">
+</svelte:head>
+
+<slot>
+    
+</slot>
