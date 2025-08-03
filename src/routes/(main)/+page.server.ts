df91f1cbfb31eb5fca4a0928163331b1c90b7e94
KiryuuinNFB <doraemonstichisnoon@gmail.com>
2025-08-04 01:22:56 +0700

    auto login

--- src/routes/(main)/+page.server.ts
+++ src/routes/(main)/+page.server.ts
@@ -1,5 +1,13 @@
 import { redirect } from "@sveltejs/kit";
+import type { PageServerLoad } from './$types';
 
-export function load() {
-    redirect(307, '/login')
+export const load: PageServerLoad = async ({ cookies }) => {
+    const token = cookies.get('jwt')
+    const user = cookies.get('user')
+    if (!token) {
+        redirect(307, "/login")
+    } else {
+        redirect(307, `/user/${user}`)
+    }
+    
 }            
